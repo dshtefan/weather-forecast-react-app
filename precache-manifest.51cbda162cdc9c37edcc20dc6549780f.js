28dc52f81034e34a584bccb1932efbe70a9d686f
Denis Shtefan <den.shtefan@gmail.com>
2019-10-22 00:20:04 +0300

    Updates

---
+++ precache-manifest.51cbda162cdc9c37edcc20dc6549780f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a175ab696c02cf7cc7866edfeb55f47",
+    "url": "/weather-search-2/index.html"
+  },
+  {
+    "revision": "18671c4005cccced573e",
+    "url": "/weather-search-2/static/css/main.0a7be384.chunk.css"
+  },
+  {
+    "revision": "e39e55e5dd8acc4c8433",
+    "url": "/weather-search-2/static/js/2.347e87c6.chunk.js"
+  },
+  {
+    "revision": "18671c4005cccced573e",
+    "url": "/weather-search-2/static/js/main.ad030b2d.chunk.js"
+  },
+  {
+    "revision": "d3629420dc5461c58b10",
+    "url": "/weather-search-2/static/js/runtime-main.461d8d1e.js"
+  },
+  {
+    "revision": "619e5992d2806086c328acb5e6c8d305",
+    "url": "/weather-search-2/static/media/delete.619e5992.svg"
+  },
+  {
+    "revision": "084487e13fa1c665f56e95ed6add2924",
+    "url": "/weather-search-2/static/media/update.084487e1.svg"
+  }
+]);
