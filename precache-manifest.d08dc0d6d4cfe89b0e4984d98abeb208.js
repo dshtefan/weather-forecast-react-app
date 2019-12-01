ff575e90a964eba18cfed01c0b25013d7736bb6a
Denis <den.shtefan@gmail.com>
2019-12-01 20:58:01 +0300

    Updates

---
+++ precache-manifest.d08dc0d6d4cfe89b0e4984d98abeb208.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df943befd11fef1e9c075c4dc63b9cd",
+    "url": "/weather-forecast-react-app/index.html"
+  },
+  {
+    "revision": "aece43e6e21a5143e8de",
+    "url": "/weather-forecast-react-app/static/css/main.02c9accb.chunk.css"
+  },
+  {
+    "revision": "d2a7bb012c5288e137d0",
+    "url": "/weather-forecast-react-app/static/js/2.4168f5a6.chunk.js"
+  },
+  {
+    "revision": "aece43e6e21a5143e8de",
+    "url": "/weather-forecast-react-app/static/js/main.fe58f050.chunk.js"
+  },
+  {
+    "revision": "59c6691a27865c0f709b",
+    "url": "/weather-forecast-react-app/static/js/runtime-main.f27dec6b.js"
+  },
+  {
+    "revision": "f8c70af5b6900586da1972f97a02a175",
+    "url": "/weather-forecast-react-app/static/media/delete.f8c70af5.svg"
+  },
+  {
+    "revision": "7917b4f1fbd2f4c81c3f2b22453b090b",
+    "url": "/weather-forecast-react-app/static/media/err.7917b4f1.svg"
+  },
+  {
+    "revision": "5befe2b8132a94ae63e8b413f2744980",
+    "url": "/weather-forecast-react-app/static/media/update.5befe2b8.svg"
+  }
+]);
